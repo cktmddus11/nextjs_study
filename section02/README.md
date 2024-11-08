b756795a128ec8e667ddeb39e96937eb97ec3618
sy.cha <sy.cha@quintet.co.kr>
2024-11-08 16:26:20 +0900

    feat(section02) : 2.9) 한입북스 UI 구현하기 - bookItem 화면 조회 UI 구현

--- section02/README.md
+++ section02/README.md
@@ -163,3 +163,4 @@ export default function App({Component, pageProps}: AppProps & {
 }) {
 
 ```
+
--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -2,17 +2,36 @@
 import style from "./index.module.css";
 import SearchableLayout from "@/components/searchable-layout";
 import {ReactNode} from "react";
+import books from '@/mock/books.json';
+import BookItem from "@/components/book-item"; // @ : src typescript
 
 export default function Home() { // Home 컴포넌트도 즉 객체이므로 getLayout 함수 추가 가능.
     return (
-        <>
-            <h1 className={style.h1}>index??</h1>
-            <h2 className={style.h2}>index??</h2>
+        <div className={style.container}>
+            <section>
+                <h3>지금 추천하는 도서</h3>
+                {
+                    books.map((book) =>
+                        <BookItem key={book.id} {...book}/>
+                    )
+                }
+            </section>
+            <section>
+                <h3>등록된 모든 도서</h3>
+                {
+                    books.map((book) =>
+                        <BookItem key={book.id} {...book}/>
+                    )
+                }
+            </section>
 
-        </>
+        </div>
     );
 }
 
 Home.getLayout = (page: ReactNode) => {
     return <SearchableLayout>{page}</SearchableLayout>
 }
+
+// <h1 className={style.h1}>index??</h1>
+// <h2 className={style.h2}>index??</h2>
